e16297896b422674a801b4fe39804a3d1292eceb
KyteKode <231786375+KyteKode@users.noreply.github.com>
2026-06-04 18:33:14 -0400

    Added title and author display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Bell Timer</h1>
+<a class="m-3 w-48 flex flex-col gap-1.5 items-center justify-center transition duration-500 hover:scale-125" href="https://github.com/KyteKode">
+    <img class="rounded-2xl" alt="" src="https://avatars.githubusercontent.com/u/231786375?s=96&;v=4">
+    Created by KyteKode
+</a>
+
